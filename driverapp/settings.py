b53c9d6050d7c36ac1f08aeb6fbc551ae8b90c88
Divmarley <kofikumi64@gmail.com>
2024-11-29 11:10:43 +0000

    sfsd

--- driverapp/settings.py
+++ driverapp/settings.py
@@ -107,23 +107,23 @@ ASGI_APPLICATION = 'driverapp.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': os.environ.get("DB_ENGINE", "django.db.backends.mysql"),
-#         'NAME': os.environ.get("MYSQL_DATABASE", 'ridderapp'),
-#         'USER': os.environ.get("MYSQL_USER", "admin"),
-#         'PASSWORD': os.environ.get("MYSQL_ROOT_PASSWORD", "ehhfaIhYCSJ7LvE"),
-#         'HOST': os.environ.get("MYSQL_HOST", "localhost"),
-#         'PORT': 3306,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get("DB_ENGINE", "django.db.backends.mysql"),
+        'NAME': os.environ.get("MYSQL_DATABASE", 'ridderapp'),
+        'USER': os.environ.get("MYSQL_USER", "admin"),
+        'PASSWORD': os.environ.get("MYSQL_ROOT_PASSWORD", "ehhfaIhYCSJ7LvE"),
+        'HOST': os.environ.get("MYSQL_HOST", "localhost"),
+        'PORT': 3306,
+    }
+}
 
 
 # Password validation
@@ -202,8 +202,8 @@ CHANNEL_LAYERS = {
 	'default': {
 		'BACKEND': 'channels_redis.core.RedisChannelLayer',
 		'CONFIG': {
-			# 'hosts': [('redis', q6379)]
-			'hosts': [('127.0.0.1', 6379)]
+			'hosts': [('redis', 6379)]
+			# 'hosts': [('127.0.0.1', 6379)]
 		},
        
  
