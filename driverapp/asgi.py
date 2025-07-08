4c2988fbeeb36ff3960b3b2ab7f18794bffc8db0
Divmarley <nexted.marley@gmail.com>
2025-07-08 00:34:40 +0000

    WIP: local changes before pulling

--- driverapp/asgi.py
+++ driverapp/asgi.py
@@ -1,25 +1,28 @@
-# asgi.py (inside driverapp directory)
-
-import chat.routing
-import food.routing
 import os
-
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
-from django.core.asgi import get_asgi_application
 
-
+# Set DJANGO_SETTINGS_MODULE before anything Django-dependent
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'driverapp.settings')
 
+# Initialize Django before importing any models/routing that uses them
 django_asgi_app = get_asgi_application()
 
+# Now it's safe to import routing that touches models or apps
+import chat.routing
+import food.routing
+
 application = ProtocolTypeRouter({
-	'http': django_asgi_app,
-	'websocket': AllowedHostsOriginValidator(
-		JWTAuthMiddlewareStack(
-			URLRouter(chat.routing.websocket_urlpatterns),
-		)
-	)
+    'http': django_asgi_app,
+    'websocket': AllowedHostsOriginValidator(
+        JWTAuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns +
+                food.routing.websocket_urlpatterns
+            )
+        )
+    )
 })
 
