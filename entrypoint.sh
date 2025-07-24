c7b7dba4765df2e7fecf8e6d923054485e989aa0
Divmarley <kofikumi64@gmail.com>
2025-07-24 00:32:56 +0000

    fill

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,61 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-# Wait for MySQL to be ready
-echo "Waiting for mysql..."
+# Wait for database
+echo "Waiting for MySQL..."
 while ! nc -z db 3306; do
-  sleep 1
+    sleep 1
 done
-echo "MySQL started"
 
-python manage.py makemigrations
+# Wait for Redis
+echo "Waiting for Redis..."
+while ! nc -z redis 6379; do
+    sleep 1
+done
+
+export DJANGO_SETTINGS_MODULE=driverapp.settings
+
+# Apply migrations
 python manage.py migrate
+
+# Collect static files
 python manage.py collectstatic --noinput
-exec gunicorn driverapp.wsgi:application --bind 0.0.0.0:8000
-exec "$@"
 
-
-# #!/bin/bash
-
-# # Ensure the database is ready before running migrations
-# mysql_ready() {
-# python << END
-# import sys
-# import MySQLdb  # mysqlclient's module
-
-# try:
-#     conn = MySQLdb.connect(
-#         host="${MYSQL_HOST}",
-#         user="${MYSQL_USER}",
-#         passwd="${MYSQL_PASSWORD}",
-#         db="${MYSQL_DATABASE}",
-#         port=int("${DB_PORT}")
-#     )
-#     conn.close()
-# except MySQLdb.OperationalError as e:
-#     print(e)
-#     sys.exit(-1)
-# sys.exit(0)
-# END
-# }
-
-# # Wait until the MySQL database is available
-# until mysql_ready; do
-#   >&2 echo 'Waiting for MySQL to become available...'
-#   sleep 1
-# done
-
-# >&2 echo 'MySQL is available'
-
-# # Run Django commands
-# python manage.py migrate
-# python manage.py makemigrations
-# python manage.py migrate
-# python manage.py collectstatic --no-input
-
-# # Start Gunicorn server
-# #exec daphne driverapp.asgi:application --bind 0.0.0.0:8000
-
-# exec python manage.py runserver 0.0.0.0:8000
-
-
-
+# Start Daphne
+exec daphne -b 0.0.0.0 -p 8000 driverapp.asgi:application
