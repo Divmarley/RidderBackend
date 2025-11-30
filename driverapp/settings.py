7b259784e056400cfe0a3585042a71387dd81e08
Divmarley <kofikumi64@gmail.com>
2025-11-30 21:06:58 +0000

    cxc

--- driverapp/settings.py
+++ driverapp/settings.py
@@ -119,24 +119,24 @@ ASGI_APPLICATION = 'driverapp.asgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get("MYSQL_DATABASE", "ridderapp"),
-        'USER': os.environ.get("MYSQL_USER", "rider_user"),
-        'PASSWORD': os.environ.get("MYSQL_PASSWORD", "securepassword"),
-        'HOST': os.environ.get("MYSQL_HOST", "db"),  # 'db' is the Docker service name
-        'PORT': os.environ.get("MYSQL_PORT", "3306"),
-         
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ.get("MYSQL_DATABASE", "ridderapp"),
+#         'USER': os.environ.get("MYSQL_USER", "rider_user"),
+#         'PASSWORD': os.environ.get("MYSQL_PASSWORD", "securepassword"),
+#         'HOST': os.environ.get("MYSQL_HOST", "db"),  # 'db' is the Docker service name
+#         'PORT': os.environ.get("MYSQL_PORT", "3306"),
+         
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
