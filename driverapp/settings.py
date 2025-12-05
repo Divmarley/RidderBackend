b91fe31f126481b0ea95facdd7aa2fcd8bfee3ae
Divmarley <kofikumi64@gmail.com>
2025-12-05 17:30:01 +0000

    sd

--- driverapp/settings.py
+++ driverapp/settings.py
@@ -229,7 +229,7 @@ CHANNEL_LAYERS = {
 		'BACKEND': 'channels_redis.core.RedisChannelLayer',
 		'CONFIG': {
 			'hosts': [('redis', 6379)],
-			'hosts': [('127.0.0.1', 6379)]
+			# 'hosts': [('127.0.0.1', 6379)]
 		},
        
  
